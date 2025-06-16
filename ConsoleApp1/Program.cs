ecf5b74f49178cf191aebb79e322bac37fc73640
Henil Maheshwari <hmi@mm-software.com>
2025-06-16 16:00:50 +0530

    Adding some changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,55 +1,75 @@
 ﻿public class Program
+{
+    public static void Main()
     {
-        public static void Main()
+        var game = new Game();
+
+        game.Roll(10);
+
+        game.Roll(10);
+
+        game.Roll(4);
+        game.Roll(0);
+
+        Console.WriteLine("Total Score: " + game.Score());
+    }
+}
+
+public class Game
+{
+    private int[] rolls = new int[21];
+    private int roll = 0;
+    private int frameRoll = 0; 
+    private int frameCount = 0;
+
+    public void Roll(int pins)
+    {
+        if (frameCount >= 10)
+            return;
+
+        rolls[roll++] = pins;
+
+        if (frameRoll == 0 && pins == 10)
         {
-            var game = new Game();
-
-            for (int i = 0; i < 1; i++)
-            {
-                game.Roll(5);
-                game.Roll(4);
-            }
-
-            Console.WriteLine("Total Score: " + game.Score());
+            rolls[roll++] = 0;
+            frameCount++;
+            frameRoll = 0;
         }
-
+        else if (frameRoll == 0)
+        {
+            frameRoll = 1;
+        }
+        else
+        {
+            frameRoll = 0;
+            frameCount++;
+        }
     }
 
-    public class Game
+    public int Score()
     {
-        private int[] rolls = new int[21];
-        private int roll = 0;
+        int score = 0;
+        int rolling = 0;
 
-        public void Roll(int pins)
+        for (int frame = 0; frame < 10; frame++)
         {
-            rolls[roll] = pins;
-            roll++;
-        }
-
-        public int Score()
-        {
-            int score = 0;
-            int rolling = 0;
-
-            for (int frame = 0; frame < 10; frame++)
+            if (rolls[rolling] == 10)
             {
-                if (rolls[rolling] == 10)
-                {
-                    score += 10 + rolls[rolling + 1] + rolls[rolling + 2];
-                    rolling += 1;
-                }
-                else if (rolls[rolling] + rolls[rolling + 1] == 10)
-                {
-                    score += 10 + rolls[rolling + 2];
-                    rolling += 2;
-                }
-                else
-                {
-                    score += rolls[rolling] + rolls[rolling + 1];
-                    rolling += 2;
-                }
+                score += 10 + rolls[rolling + 2] + rolls[rolling + 3];
+                rolling += 2;
             }
-
-            return score;
+            else if (rolls[rolling] + rolls[rolling + 1] == 10)
+            {
+                score += 10 + rolls[rolling + 2];
+                rolling += 2;
+            }
+            else
+            {
+                score += rolls[rolling] + rolls[rolling + 1];
+                rolling += 2;
+            }
         }
+
+        return score;
     }
+}
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be0a2f5a9785f2a13584b002dbc54f65873ab80")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
